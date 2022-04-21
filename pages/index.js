37cdf226c3f36bb90977ee2e775d1e813623aa21
Hristo Shirov <hristo.shirov@mentormate.com>
2022-04-21 20:02:15 +0300

    Update progress 21/04/22

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const meetups = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address',
+        description: 'This is the first meetup'
+    }
+];
+
 const HomePage = () => {
-    return (
-        <h1>Home Page</h1>
-    );
+    return <MeetupList meetups={meetups} />;
 }
 
 export default HomePage;
